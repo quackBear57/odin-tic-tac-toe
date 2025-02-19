fa7a5c17690a24aac974afd5b2d92470662eac64
quackBear57 <89419185+quackBear57@users.noreply.github.com>
2025-02-19 11:10:12 -0600

    initial game logic attempt

---
+++ javascript.js
@@ -0,0 +1,51 @@
+function gameboard() {
+    const rows = 3;
+    const cols = 3;
+    const board = [];
+
+    for (let i = 0; i < rows; i++) {
+        board[i] = [];
+        for (let j = 0; i < cols; i++) {
+            board[i].push(square());
+        }
+    }
+
+    const getBoard = () => board;
+
+    const markSquare = (row, column, player) => {
+        if (!board[row][column].getValue() === '') {
+            board[row][column].addMark();
+        } else {
+            return;
+        }
+    };
+
+    const printBoard = () => {
+        const boardWithMarks = board.map((row) => row.map((cell) => cell.getValue()))
+        console.log(boardWithMarks);
+    };
+
+    return {
+        getBoard,
+        markSquare,
+        printBoard
+    };
+}
+
+function square() {
+    let value = '';
+
+    const addMark = (player) => {
+        value = player;
+    };
+
+    const getValue = () => value;
+
+    return {
+        addMark,
+        getValue
+    };
+}
+
+gameboard();
+gameboard.printBoard();
